253f4c8430a496ee821c91741a5bfc633a00155e
TS93 <tschram93@yahoo.com>
2022-04-29 17:11:19 -0400

    created Cuisine.jsx page imported it into pages; created a route with it

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,13 +1,15 @@
+import Cuisine from './Cuisine';
 import Home from './Home';
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function Pages() {
-    return (
-        <Routes>
-        <Route element={<Home/>} path='/' />
-        </Routes>
-    )
+	return (
+		<Routes>
+			<Route element={<Home />} path='/' />
+			<Route element={<Cuisine />} path='/cuisine' />
+		</Routes>
+	);
 }
 
 export default Pages;
