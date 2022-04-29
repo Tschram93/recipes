4b8f82510ad4799325084daa724b994436831b05
TS93 <tschram93@yahoo.com>
2022-04-29 16:45:00 -0400

    imported navlink into category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import * as FaIcons from 'react-icons/fa';
 import * as GiIcons from 'react-icons/gi';
+import {NavLink} from 'react-router-dom';
 
 function Category() {
     return (
         <div>
-            Category
+            
         </div>
     )
 };
