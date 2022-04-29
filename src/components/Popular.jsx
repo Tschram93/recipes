8957cea474de32f9133a3454794ef318561be6f0
TS93 <tschram93@yahoo.com>
2022-04-29 12:00:05 -0400

    updated .env to match react requirements

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Popular = () => {
+    useEffect(() => {
+        // Runs getPopular function as soon as the component is mounted
+        getPopular();
+    }, [])
     const getPopular = async () => {
         // Fetch api and utilizing .env file for security of the api key information
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=8`)
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`)
         // Gives json format for the api data to interact with
         const data = await api.json();
         console.log(data);
