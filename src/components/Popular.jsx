61cfc66136bbc62789d3cbf6c399d0a3be89e141
TS93 <tschram93@yahoo.com>
2022-04-29 12:04:19 -0400

    utilizing useState within popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Popular = () => {
+    const [popular, setPopular] = useState([]);
+
     useEffect(() => {
         // Runs getPopular function as soon as the component is mounted
         getPopular();
-    }, [])
+    }, []);
+
     const getPopular = async () => {
         // Fetch api and utilizing .env file for security of the api key information
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`)
