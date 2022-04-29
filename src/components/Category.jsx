a6b8ca021c77a9b9afd32e4cd1a4f88da8a78514
TS93 <tschram93@yahoo.com>
2022-04-29 16:58:07 -0400

    created styled-component 'List' for individual divs containing food types

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,7 +6,7 @@ import {NavLink} from 'react-router-dom';
 
 function Category() {
     return (
-        <div>
+        <List>
             <div>
                 <FaIcons.FaPizzaSlice />
                 <h4>Italian</h4>
@@ -23,8 +23,15 @@ function Category() {
                 <GiIcons.GiChopsticks />
                 <h4>Japanese</h4>
             </div>
-        </div>
+        </List>
     )
 };
+
+
+const List = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 2rem 0rem;
+`;
 
 export default Category;
