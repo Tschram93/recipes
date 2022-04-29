733c998520111e08aeea39cf9c431c4f1214b20e
TS93 <tschram93@yahoo.com>
2022-04-29 12:20:44 -0400

    creating div for each recipe from each api result using map, then displaying recipe title

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
 const Popular = () => {
-    const [popular, setPopular] = useState([]);
+	const [popular, setPopular] = useState([]);
 
-    useEffect(() => {
-        // Runs getPopular function as soon as the component is mounted
-        getPopular();
-    }, []);
+	useEffect(() => {
+		// Runs getPopular function as soon as the component is mounted
+		getPopular();
+	}, []);
 
-    const getPopular = async () => {
-        // Fetch api and utilizing .env file for security of the api key information
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`)
-        // Gives json format for the api data to interact with
-        const data = await api.json();
-        console.log(data);
-    }
+	const getPopular = async () => {
+		// Fetch api and utilizing .env file for security of the api key information
+		const api = await fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`
+		);
+		// Gives json format for the api data to interact with
+		const data = await api.json();
+		setPopular(data.recipes);
+	};
 
-    return (
-        <div>Popular</div>
-    )
-}
+	return (
+		<div>
+			{/* using map to go through the API json data and outputting a recipe title
+			 */}
+			{popular.map((recipe) => {
+				return (
+					<div key={recipe.id}>
+                        {/* recipe.title from api; needs key to avoid errors */}
+						<p>{recipe.title}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default Popular;
